313bc686b1faec70ff2187f383eb38e8cb108ec5
LUFENGZHAN <xiaozhan2000423@163.com>
2022-08-12 12:29:35 +0800

    [add] add

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Table from './page/Table'
+import Table from './components/Table'
 import App from './App'
 import log from './assets/images/logo.svg'
 import './assets/css/index.css';
