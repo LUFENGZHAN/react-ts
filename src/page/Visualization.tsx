2bd388e52f34a06411007b75addaeafbb093d12c
LUFENGZHAN <xiaozhan2000423@163.com>
2022-09-06 16:53:12 +0800

    [add] add

--- src/page/Visualization.tsx
+++ src/page/Visualization.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import  style  from './scss/Visualization.module.scss'
+import  api  from '../service/request'
+api.get('/data').then(res =>{
+  console.log(res);
+  
+})
 export default function Visualization() {
     const to = useNavigate()
   return (
--- src/service/request.ts
+++ src/service/request.ts
@@ -1,4 +1,15 @@
-import {ipList} from "./ipconfig";
+import { ipList } from "./ipconfig";
+
 import axios from "axios";
-const api = axios.create({baseURL: ipList.PRESENT_IP, timeout: ipList.TIME});
+const api = axios.create({ baseURL: ipList.PRESENT_IP, timeout: ipList.TIME });
+api.interceptors.request.use(
+    (config) => {
+        let token = localStorage.getItem("token")
+
+        if (token && config.headers) {
+            config.headers['token'] = token
+        }
+        return config;
+    },
+)
 export default api;
