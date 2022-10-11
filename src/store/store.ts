80951f238edd59ef8b8411cceab0f7d190028378
LUFENGZHAN <xiaozhan2000423@163.com>
2022-10-11 17:25:26 +0800

    [add] add

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,17 @@
 import {createStore} from 'redux'
 import {reducer} from './reducer'
 
+class ReducerStore {
+    count = 0
+    constructor(a: any){
+        this.count = a
+        
+    }
+    // class 计算方法 get
+    get add(){
+        return this.count
+    }
+    
+}
+export default ReducerStore
 export const store = createStore(reducer)
