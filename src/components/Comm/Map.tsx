50632f1eeb4bd5ce4bd92d9457dc79664cd571ef
LUFENGZHAN <xiaozhan2000423@163.com>
2022-10-15 21:30:26 +0800

    add

--- src/components/Comm/Map.tsx
+++ src/components/Comm/Map.tsx
@@ -12,17 +12,34 @@ function AMap(data?: any){
     let map: any
     let AMap = window.AMap
     map = new AMap.Map('map', {
-        zoom: 17,
-        pitch: 50,
+        zoom: 16,
+        pitch: 44,
         rotation: 10,
         showBuildingBlock: true,
         showIndoorMap: true,
-        showLabel: true,
+        showLabel: false,
         animateEnable: true,
         // 主题
         mapStyle: "amap://styles/blue",
         features: ["bg", "point", "road"],
         viewMode: "3D",
     });
+    var ellipse = new AMap.Ellipse({
+        center: [121.55, 29.87],
+        radius: [ 500, 500 ], //半径
+        borderWeight: 0,
+        strokeColor: "#FF33FF", 
+        strokeOpacity: 1,
+        strokeWeight: 1,
+        fillOpacity: 0.4,
+        // 线样式还支持 'dashed'
+        strokeStyle: 'dashed',
+        strokeDasharray: [20, 10], 
+        fillColor: '#1791fc',
+        zIndex: 50,
+    })
+
+    map.add(ellipse);
+    map.setFitView(null, false, [100, 60, 100, 60]);        
         
 }
